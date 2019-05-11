af39cabc04cd836723d2fe796660244b7782b1af
danpatrickoneill <dan47573@gmail.com>
2019-05-11 17:32:32 -0400

    Implemented user addition and deletion

--- src/main/java/com/lambdaschool/todo/controller/UserController.java
+++ src/main/java/com/lambdaschool/todo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.lambdaschool.todo.controller;
 
+import com.lambdaschool.todo.model.ToDo;
 import com.lambdaschool.todo.model.User;
 
 import com.lambdaschool.todo.service.UserService;
@@ -7,9 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -22,5 +24,37 @@ public class UserController
     public ResponseEntity<?> getPostsByUser()
     {
         return new ResponseEntity<>(userService.findToDosByUser(), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers()
+    {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PostMapping
+    public ResponseEntity<?> addNewUser(@Valid @RequestBody User newUser)
+    {
+        newUser = userService.save(newUser);
+
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/todo/{userid}")
+    public ResponseEntity<?> addNewUser(@RequestBody ToDo newToDo, @PathVariable long userid)
+    {
+        newToDo = null;
+
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteUserById(@PathVariable long id)
+    {
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
